4ed5864cf379cad92a18c98baf3bdda92a9e99ea
sushil-mod <78729065+sushil-mod@users.noreply.github.com>
2021-09-30 14:49:58 +0530

    final logic

--- app.js
+++ app.js
@@ -5,19 +5,26 @@ var checkBtn = document.querySelector("#check-button");
 var quantityStock = document.querySelector("#quantity-of-stock");
 var result = document.querySelector("#output");
 
+checkBtn.addEventListener('click',clickHandlerStock);
 
+function clickHandlerStock(){
+
+    profitAndLoss(Number(purchasePrice.value),Number(quantityStock.value),Number(currentPrice.value));
+}
+
+ 
 function profitAndLoss(purchase,quantity,current){
 
     if(purchase>current){
         var loss = (purchase-current)*quantity;
         var lossPercentage = (loss/purchase)*100;
-        console.log("loss");
+
+        result.innerText = 'you are in loss of ' +loss+' and loss percentage  is '+lossPercentage ;
     }else if(current>purchase){
         var profit = (current-purchase)*quantity;
         var profitPercentage = (profit/purchase)*100;
-        console.log("profit");
+        result.innerText = 'you are in loss of ' +profit +' and loss percentage  is '+profitPercentage ;
     }else{
-        console.log("same");
+        result.innerText = 'Still No gains' ;
     }
 }
-profitAndLoss(100,10,10);
