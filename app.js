3233a275b657b5afd3ae14111d688286426f186f
sushil-mod <78729065+sushil-mod@users.noreply.github.com>
2021-09-30 13:55:54 +0530

    linked i/o to js

---
+++ app.js
@@ -0,0 +1,8 @@
+
+var purchasePrice = document.querySelector("#purchase-price");
+var currentPrice = document.querySelector("#current-price");
+var checkBtn = document.querySelector("#check-button");
+var quantityStock = document.querySelector("#quantity-of-stock");
+var result = document.querySelector("#output");
+
+
