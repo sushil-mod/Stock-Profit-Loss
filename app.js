097ec455cc91226cc800cf8e442d6675d94a4e8d
sushil-mod <78729065+sushil-mod@users.noreply.github.com>
2021-09-30 14:10:36 +0530

    dry run logic

--- app.js
+++ app.js
@@ -6,3 +6,18 @@ var quantityStock = document.querySelector("#quantity-of-stock");
 var result = document.querySelector("#output");
 
 
+function profitAndLoss(purchase,quantity,current){
+
+    if(purchase>current){
+        var loss = (purchase-current)*quantity;
+        var lossPercentage = (loss/purchase)*100;
+        console.log("loss");
+    }else if(current>purchase){
+        var profit = (current-purchase)*quantity;
+        var profitPercentage = (profit/purchase)*100;
+        console.log("profit");
+    }else{
+        console.log("same");
+    }
+}
+profitAndLoss(100,10,10);
