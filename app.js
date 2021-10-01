639ca6e641a78b3724ffb3cdd7f60c679cf1d27b
sushil-mod <78729065+sushil-mod@users.noreply.github.com>
2021-10-01 19:52:37 +0530

    style:minor changes

--- app.js
+++ app.js
@@ -5,25 +5,43 @@ var checkBtn = document.querySelector("#check-button");
 var quantityStock = document.querySelector("#quantity-of-stock");
 var result = document.querySelector("#output");
 
+var imageMain = document.querySelector(".main-img");
+var imageProfit = document.querySelector(".profit-img");
+var imageLoss = document.querySelector(".loss-img");
+
 checkBtn.addEventListener('click',clickHandlerStock);
 
 function clickHandlerStock(){
 
+   if (Number(purchasePrice.value)>0 || Number(currentPrice.value)>0 || Number(quantityStock.value>0) )
+   {  
+       
     profitAndLoss(Number(purchasePrice.value),Number(quantityStock.value),Number(currentPrice.value));
+}else{
+    result.innerText = "Enter Valid Amount";
 }
+
+   
+}
+
+  
 
  
 function profitAndLoss(purchase,quantity,current){
 
     if(purchase>current){
-        var loss = (purchase-current)*quantity;
-        var lossPercentage = (loss/purchase)*100;
+        var loss = ((purchase-current)*quantity).toFixed(2);
+        var lossPercentage = ((loss/purchase)*100).toFixed(2);
+        imageMain.style.display = "none"; 
+        result.innerText = 'you are in loss of ' +loss+' and loss percentage  is '+lossPercentage;
+        imageLoss.style.display = "block";
 
-        result.innerText = 'you are in loss of ' +loss+' and loss percentage  is '+lossPercentage ;
     }else if(current>purchase){
-        var profit = (current-purchase)*quantity;
-        var profitPercentage = (profit/purchase)*100;
-        result.innerText = 'you are in loss of ' +profit +' and loss percentage  is '+profitPercentage ;
+        var profit = ((current-purchase)*quantity).toFixed(2);
+        var profitPercentage = ((profit/purchase)*100).toFixed(2);
+        imageMain.style.display = "none"; 
+        result.innerText = 'you are in Profit of ' +profit +' and Profit percentage  is '+profitPercentage ;
+        imageProfit.style.display = "block";
     }else{
         result.innerText = 'Still No gains' ;
     }
